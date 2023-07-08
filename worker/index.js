519d94a39182264237a798d1f2e16f167833c34f
Andy Chen <amazingandyyy@gmail.com>
2023-07-07 22:13:45 -0700

    styles: use standard --fix

--- worker/index.js
+++ worker/index.js
@@ -1,42 +1,49 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const path = require('path');
-const fs = require('fs');
-const rdiff = require('recursive-diff');
+const axios = require('axios')
+const cheerio = require('cheerio')
+const path = require('path')
+const fs = require('fs')
+const rdiff = require('recursive-diff')
 
 const existingSnapshot = require('../api/developments/snapshot.json')
 const existingLogs = require('../api/developments/logs.json')
 
-const site_url = process.env.TARGET || 'https://dublin-development.icitywork.com'
-const latest_output_path = process.env.LATEST_OUTPUT || path.join(__dirname, '../api/', 'developments/snapshot.json')
-const logs_output_path = process.env.LOGS_OUTPUT || path.join(__dirname, '../api/', 'developments/logs.json')
+function main ({
+  siteUrl,
+  latestOutputPath,
+  logsOutputPath,
+  enableLogs
+}) {
+  siteUrl = siteUrl || 'https://dublin-development.icitywork.com'
+  latestOutputPath = latestOutputPath || path.join(__dirname, '../api/', 'developments/snapshot.json')
+  logsOutputPath = logsOutputPath || path.join(__dirname, '../api/', 'developments/logs.json')
+  enableLogs = process.env.ENABLE_LOGS === 'true' || enableLogs || false
 
-axios.get(site_url)
-  .then(res=>{
+  axios.get(siteUrl)
+    .then(res => {
       // const input = fs.readFileSync(path.join(__dirname, '..','docs/index-2.html'))
-
-      const input = res.data;
+      const input = res.data
       const [data, diff] = build(input)
       // console.log(JSON.stringify(data, null, 2))
-      fs.writeFileSync(latest_output_path, JSON.stringify(data, null, 2))
-      
-      if(process.env.ENABLE_LOGS === 'true' && diff.length>0){
-        fs.writeFileSync(logs_output_path, JSON.stringify([...existingLogs, ...diff], null, 2))
-      }
-  })
-  .catch(console.error)
+      fs.writeFileSync(latestOutputPath, JSON.stringify(data, null, 2))
 
-const normalize = (str) => {
-  return str.replace(/\s+/g, ' ').trim();
+      if (enableLogs && diff.length > 0) {
+        fs.writeFileSync(logsOutputPath, JSON.stringify([...existingLogs, ...diff], null, 2))
+      }
+    })
+    .catch(console.error)
 }
 
-function build(HTML) {
-  const $ = cheerio.load(HTML);
-  const projectIDs = [];
+const normalize = (str) => {
+  return str.replace(/\s+/g, ' ').trim()
+}
+
+function build (HTML) {
+  const $ = cheerio.load(HTML)
+  const projectIDs = []
 
   const detailsFns = {
     Applicant: (el) => {
-      const i = $(el).find('.table-right').html().trim().split('<br>').map(normalize);
+      const i = $(el).find('.table-right').html().trim().split('<br>').map(normalize)
       let r = {}
       if (i.length === 3) {
         r = {
@@ -52,7 +59,7 @@ function build(HTML) {
             address: i[1],
             phone: 'n/a'
           }
-        } else{
+        } else {
           r = {
             name: i[0],
             address: 'n/a',
@@ -60,97 +67,97 @@ function build(HTML) {
           }
         }
       }
-      return r;
+      return r
     },
     'Project Planner': (el) => {
-      const i = $(el).find('.table-right').html().trim().split('<br>').map(normalize);
+      const i = $(el).find('.table-right').html().trim().split('<br>').map(normalize)
       return {
         name: i[0],
         phone: i[1],
-        email: normalize($(el).find('.table-right').find('a').text()),
+        email: normalize($(el).find('.table-right').find('a').text())
       }
-    },
+    }
   }
 
   $('.modal.fade').each((i, el) => {
     projectIDs.push(el.attribs.id)
   })
 
-  function parseGeo() {
-    const data = {};
+  function parseGeo () {
+    const data = {}
     $('script[type="text/javascript"]').each((index, element) => {
-      const scriptContent = $(element).html();
-      const latMatch = scriptContent.match(/var lat = (.*?);/);
-      const lonMatch = scriptContent.match(/var lon = (.*?);/);
-      const postidMatch = scriptContent.match(/var postid =(.*?);/);
-      const iconNameMatch = scriptContent.match(/var iconName = "(.*?)";/);
+      const scriptContent = $(element).html()
+      const latMatch = scriptContent.match(/var lat = (.*?);/)
+      const lonMatch = scriptContent.match(/var lon = (.*?);/)
+      const postidMatch = scriptContent.match(/var postid =(.*?);/)
+      const iconNameMatch = scriptContent.match(/var iconName = "(.*?)";/)
 
       if (latMatch && lonMatch && postidMatch) {
-        const lat = parseFloat(latMatch[1]);
-        const lon = parseFloat(lonMatch[1]);
-        const postid = parseInt(postidMatch[1]);
-        const iconName = iconNameMatch[1];
+        const lat = parseFloat(latMatch[1])
+        const lon = parseFloat(lonMatch[1])
+        const postid = parseInt(postidMatch[1])
+        const iconName = iconNameMatch[1]
 
         const item = {
           lat,
           lon,
-          iconName,
-        };
-        data[`projectDetail${postid}`] = item //)
+          iconName
+        }
+        data[`projectDetail${postid}`] = item // )
       }
-    });
+    })
 
     return data
   }
 
-  const geoLocations = parseGeo();
-  const data = existingSnapshot;
-  const diff = [];
+  const geoLocations = parseGeo()
+  const data = existingSnapshot
+  const diff = []
   projectIDs.forEach((id) => {
     $(`#${id}`).each((i, el) => {
-      const d={};
-      d.id = id;
-      d.title = $(el).find('#myModalLabel').text();
+      const d = {}
+      d.id = id
+      d.title = $(el).find('#myModalLabel').text()
       d.details = {}
       $(el).find('table').find('tr').each((i, el) => {
-        const key = normalize($(el).find('.table-left').find('strong').text());
-        const val = normalize($(el).find('.table-right').text());
+        const key = normalize($(el).find('.table-left').find('strong').text())
+        const val = normalize($(el).find('.table-right').text())
         d.details[key] = val
-        if(detailsFns[key] !== undefined) {
+        if (detailsFns[key] !== undefined) {
           d.details[key] = detailsFns[key](el)
         }
       })
 
-      function findBlockByTitle(title) {
-        return $(el).find('h4.dublin-green').filter((i, el) => $(el).text()===title)[0]
+      function findBlockByTitle (title) {
+        return $(el).find('h4.dublin-green').filter((i, el) => $(el).text() === title)[0]
       }
 
-      d.location = normalize(findBlockByTitle('Address')?.nextSibling.nodeValue);
-      const docs = $(el).find('h4.dublin-green')[3];
-      function cleanUpCityDocsUrl(url) {
+      d.location = normalize(findBlockByTitle('Address')?.nextSibling.nodeValue)
+      const docs = $(el).find('h4.dublin-green')[3]
+      function cleanUpCityDocsUrl (url) {
         return url
           .replace('http://citydocs.ci.dublin.ca.us/', 'https://citydocs.dublin.ca.gov/')
           .replace('https://dublin-development.icitywork.com/', '')
           .replace('wp-content/', 'https://dublin-development.icitywork.com/wp-content/')
       }
-      function cleanUpImagesUrl(url) {
+      function cleanUpImagesUrl (url) {
         return url
           .replace('https://dublin-development.icitywork.com/', '')
           .replace('wp-content/', 'https://dublin-development.icitywork.com/wp-content/')
       }
-      function traverseSiblings(el, acc=[]) {
+      function traverseSiblings (el, acc = []) {
         if ($(el).next().find('a').attr('href') !== undefined) {
           return traverseSiblings($(el).next(), [...acc, {
             name: normalize($(el).next().text()),
             url: cleanUpCityDocsUrl($(el).next().find('a').attr('href'))
           }])
-        }else{
-          return acc;
+        } else {
+          return acc
         }
       }
       d.docs = traverseSiblings(docs.nextSibling)
-      d.status = $(findBlockByTitle('Status')).next().attr('alt');
-      d.description = normalize($(findBlockByTitle('Project Description')).next().text());
+      d.status = $(findBlockByTitle('Status')).next().attr('alt')
+      d.description = normalize($(findBlockByTitle('Project Description')).next().text())
       const images = findBlockByTitle('Project Images').nextSibling
       d.images = $(images).next().find('li').map((i, el) => ({
         original: cleanUpImagesUrl($(el).find('a').attr('href')),
@@ -158,30 +165,35 @@ function build(HTML) {
       })).get()
 
       d.geolocation = {
-        lat: null, lon: null, iconName: 'dot', title: d.title,
+        lat: null,
+        lon: null,
+        iconName: 'dot',
+        title: d.title,
         ...geoLocations[d.id]
       }
 
-      if(data[d.id] !== undefined) {
+      if (data[d.id] !== undefined) {
         // it exists, find the diff and put the activity into the feed
         const localDiff = rdiff.getDiff(data[d.id], d, true)
-        function cleanUpDiff(diff) {
-          return diff.map(dif=>{
+        function cleanUpDiff (diff) {
+          return diff.map(dif => {
             return {
               ...dif,
               projectId: d.id,
-              timestamp: Date.now(),
+              timestamp: Date.now()
             }
           })
         }
-        if(localDiff.length > 0) diff.push(...cleanUpDiff(localDiff))
+        if (localDiff.length > 0) diff.push(...cleanUpDiff(localDiff))
 
-        data[d.id] = d;
-      }else{
-        d.createdAt = new Date().toISOString().split('T')[0];
-        data[d.id] = d;
+        data[d.id] = d
+      } else {
+        d.createdAt = new Date().toISOString().split('T')[0]
+        data[d.id] = d
       }
     })
   })
-  return [data, diff];
+  return [data, diff]
 }
+
+module.exports = main
