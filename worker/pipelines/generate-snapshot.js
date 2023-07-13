19d6d5db77e1038778b6b48c3878ed8b843aed33
Andy Chen <amazingandyyy@gmail.com>
2023-07-13 12:18:04 -0700

    feat: don't duplicate snapshot

--- worker/pipelines/generate-snapshot.js
+++ worker/pipelines/generate-snapshot.js
@@ -1,22 +1,28 @@
 const axios = require('axios')
-
-const { writeToFileForce, generateValidDatekey, absolutePath } = require('../utils')
+const fs = require('fs')
+const { writeToFileForce, generateValidDatekey, absolutePath, logger } = require('../utils')
 const { generateDateKeys } = require('../utils/generator')
 
 const target = 'https://dublin-development.icitywork.com/'
 
 async function main () {
   const datekey = generateValidDatekey(new Date())
-  await generateHtml(datekey)
-  await generateDateKeys()
+  const updated = await generateHtml(datekey)
+  if (updated) await generateDateKeys()
 }
 
 const generateHtml = async (datekey) => {
   let html = await axios.get(target)
   html = html.data.replace(/\?wordfence_lh=1&hid=.*/i, '')
+  const previousDatekey = fs.readFileSync(absolutePath('docs/archive/datekeys-latest.txt'), 'utf8')
+  const previousHtml = fs.readFileSync(absolutePath(`docs/archive/${previousDatekey}/dublin-development.icitywork.com/index.html`), 'utf8')
+  if (previousHtml === html) {
+    logger.info('No page changes detected, skipping snapshot')
+    return false
+  }
   const p = absolutePath(`docs/archive/${datekey}/dublin-development.icitywork.com/index.html`)
   writeToFileForce(p, html)
-  return html
+  return true
 }
 
 main()
