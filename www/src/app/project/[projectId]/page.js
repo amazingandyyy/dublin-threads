8da143dc9fd9fef9fdf21deee4d6d0dc231d6a82
Andy Chen <amazingandyyy@gmail.com>
2023-07-27 19:40:08 -0700

    feat: update detail page

--- www/src/app/project/[projectId]/page.js
+++ www/src/app/project/[projectId]/page.js
@@ -6,7 +6,7 @@ export default function Project ({ params }) {
   return (<>
     <GlobalHeader />
     <main className="flex flex-col items-center bg-white h-screen w-screen pt-16 md:p-4 md:pt-[82px]">
-      <div className='bg-gray-100 rounded-xl flex max-w-[750px] w-full p-4'>
+      <div>
         <ProjectDetails projectId={params.projectId} />
       </div>
     </main>
