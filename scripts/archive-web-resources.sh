87c21f0727e891133b8a97f75433e062e4ed503e
Andy Chen <amazingandyyy@gmail.com>
2024-06-18 17:06:01 -0700

    feat: add more assets

--- scripts/archive-web-resources.sh
+++ scripts/archive-web-resources.sh
@@ -6,7 +6,8 @@ OUTPUT_DIR=$ROOT_DIR/docs/web-archive
 mkdir -p $OUTPUT_DIR
 
 echo "Archiving $target_url to $OUTPUT_DIR"
-wget --no-clobber --recursive --no-host-directories --page-requisites -P $OUTPUT_DIR https://$target_url
+# wget --no-clobber --recursive --no-host-directories --page-requisites -P $OUTPUT_DIR https://$target_url
+wget --recursive --no-host-directories --page-requisites -P $OUTPUT_DIR https://$target_url
 
 git stash push docs/web-archive/wp-content/uploads/2016/05/DublinSeal144x144.jpg
 git stash push docs/web-archive/wp-content/uploads/2016/05/DublinSeal114x114.jpg
